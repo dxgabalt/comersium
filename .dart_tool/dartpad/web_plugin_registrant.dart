0f03f6a2b8151cb553d997d43f576251e54adc0e
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-11-02 17:29:32 -0600

    Eliminar archivos de configuración obsoletos y actualizar dependencias de Firebase

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:firebase_performance_web/firebase_performance_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
@@ -30,6 +31,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FilePickerWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
   FirebasePerformanceWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
   FlutterFacebookAuthPlugin.registerWith(registrar);
